4c626ce03255a0f012e1106626a92e0951f3340b
httpdaniel <daniel.097@hotmail.com>
2020-10-28 17:23:32 +0000

    Initial CreateIndex

--- src/main/java/CreateIndex.java
+++ src/main/java/CreateIndex.java
@@ -1,5 +1,44 @@
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
 public class CreateIndex {
-    public static void main(String[] args) {
-        System.out.println("test1");
+
+    // Directory where the search index will be saved
+    private static String INDEX_DIRECTORY = "../index";
+
+    public static void main (String[] args) throws IOException {
+
+        // Analyzer that is used to process text field
+        Analyzer analyzer = new StandardAnalyzer();
+
+        // Store index on disk
+        Directory directory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+
+        // Set new open mode to create new index
+        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+
+        IndexWriter iwriter = new IndexWriter(directory, config);
+
+        // TO-DO Create a new document
+        Document doc = new Document();
+
+        // Save document to index
+        iwriter.addDocument(doc);
+
+        // Commit changes and close
+        iwriter.close();
+        directory.close();
+
     }
 }
